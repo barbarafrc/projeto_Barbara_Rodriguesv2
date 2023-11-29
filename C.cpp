70089e548863c8fb28f4205bc7dc190c6046ffa2
barbara-rodrigues <barbara.rodrigues@dtidigital.com.br>
2023-11-29 19:11:19 -0300

    alteração na classe C

--- C.cpp
+++ C.cpp
@@ -27,6 +27,6 @@ void C::MC2() {
     std::cout << "Método MC2() chamado." << std::endl;
 }
 
-void C::MC3() {
-    std::cout << "Método MC2() chamado." << std::endl;
-}
+ void C::MC3() {
+     std::cout << "Método MC3() chamado." << std::endl;
+ }
